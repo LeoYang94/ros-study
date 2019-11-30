ac0e4a8712d7faeadf086d532af570cf51986917
LeoYang94 <liyang_s@cumt.edu.cn>
2019-11-30 15:01:53 +0800

    add explain

--- pluginlib_tutorials_/src/polygon_plugins.cc
+++ pluginlib_tutorials_/src/polygon_plugins.cc
@@ -1,6 +1,7 @@
 #include <pluginlib/class_list_macros.h>
 #include <pluginlib_tutorials_/polygon_base.h>
 #include <pluginlib_tutorials_/polygon_plugins.h>
- 
+
+//标记为导出类
 PLUGINLIB_EXPORT_CLASS(polygon_plugins::Triangle, polygon_base::RegularPolygon)
 PLUGINLIB_EXPORT_CLASS(polygon_plugins::Square, polygon_base::RegularPolygon)
